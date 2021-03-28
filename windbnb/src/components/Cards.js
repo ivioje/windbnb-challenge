286c2169bc2191c0d3150f2681fa49f24ec46831
ivioje <rhodaivioje@gmail.com>
2021-03-28 20:34:14 +0100

    css style for the cards

--- windbnb/src/components/Cards.js
+++ windbnb/src/components/Cards.js
@@ -1,22 +1,29 @@
 import React from 'react';
 
 const Cards = () => {
-    return ( 
-        <div className='container'>
-            <h2>Stays in Finland</h2>
-<div className='card_layout'>
-    <div className='card_items'>
-        <img src={`https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80`} alt='card' />
-        <div className='card_des'>
-            <button><p>SUPER HOST</p></button>
+  return (
+    <div className='container'>
+      <h2>Stays in Finland</h2>
+      <h3>12+ stays</h3>
+      <div className='card_layout'>
+        <div className='card_items'>
+          <img
+            src={`https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80`}
+            alt='card'
+          />
+          <div className='card_des'>
+            <button>
+              <p>SUPER HOST</p>
+            </button>
             <p2>Entire apartment. 2 beds</p2>
-            <p3> 4.40</p3>
+            <p3><span>	
+&#9734;</span> 4.40</p3>
+          </div>
+          <h4>Stylist apartment in center of the city</h4>
         </div>
-        <h4>Stylist apartment in center of the city</h4>
+      </div>
     </div>
-</div>
-        </div>
-     );
-}
- 
+  );
+};
+
 export default Cards;
