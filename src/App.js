80400ff52d79fc5e26b16f3c1797cd35c78ebe49
ivioje <rhodaivioje@gmail.com>
2021-04-10 10:43:27 +0100

    filter component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Stays from './components/Stays.js';
 import { stays } from './stays.js';
 import './index.css';
+import Filter from './components/Filter.js';
 
 const App = () => {
   return (
     <div className='App'>
+      <Filter />
       <div className='stays'>
         <h2>Stays in Finland</h2>
         <h5>{stays.length > 12 ? '12+ stays' : stays.length + ' stays'}</h5>
---
+++ src/components/Filter.js
@@ -0,0 +1,20 @@
+import react from 'react';
+
+
+const Filter = () => {
+  return (
+    <div>
+      <div className='stays_location'>
+        <h5>Helsink, Finland</h5>
+      </div>
+      <div className='guest'>
+          <h5>Add guests</h5>
+      </div>
+      <div className='search_icon'>
+          <h5>ICO</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
