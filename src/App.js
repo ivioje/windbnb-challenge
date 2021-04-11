85f6e05f8a6434e3ee65b41b61a3dd71198d2ff5
ivioje <rhodaivioje@gmail.com>
2021-04-11 21:50:50 +0100

    filter drawer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import Filter from './components/Filter.js';
 const App = () => {
   return (
     <div className='App'>
+      <div className='header'>
+<div>Logo</div>
       <Filter />
+      </div>
       <div className='stays'>
         <h2>Stays in Finland</h2>
         <h5>{stays.length > 12 ? '12+ stays' : stays.length + ' stays'}</h5>
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,17 +1,17 @@
 import react from 'react';
 
-
 const Filter = () => {
   return (
-    <div>
-      <div className='stays_location'>
+    <div className='filter'>
+      <div className='stays_location drawer'>
         <h5>Helsink, Finland</h5>
       </div>
-      <div className='guest'>
-          <h5>Add guests</h5>
+
+      <div className='guest drawer'>
+        <h5>Add guests</h5>
       </div>
-      <div className='search_icon'>
-          <h5>ICO</h5>
+      <div className='search_icon drawer'>
+        <h5>ICO</h5>
       </div>
     </div>
   );
