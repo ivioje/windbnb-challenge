fecc0ce8cdcc1f8d8660b1666724b6e214db7bfe
ivioje <rhodaivioje@gmail.com>
2021-04-12 15:42:54 +0100

    filter drawer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import Stays from './components/Stays.js';
-import { stays } from './stays.js';
-import './index.css';
-import Filter from './components/Filter.js';
+import Components from './components/Components.js'
 
 const App = () => {
   return (
     <div className='App'>
-      <div className='header'>
-<div>Logo</div>
-      <Filter />
-      </div>
-      <div className='stays'>
-        <h2>Stays in Finland</h2>
-        <h5>{stays.length > 12 ? '12+ stays' : stays.length + ' stays'}</h5>
-      </div>
-      <Stays location={stays} />
+    <Components />
     </div>
   );
 };
---
+++ src/components/Components.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Stays from './Stays.js';
+import { stays } from '../stays.js';
+import '../index.css';
+import logo from '../assets/logo.svg';
+import Filter from './Filter.js';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='header'>
+        <div className='logo'>
+          <img src={logo} alt='windbnb_logo' />
+        </div>
+        <Filter />
+      </div>
+      <div className='stays'>
+        <h2>Stays in Finland</h2>
+        <h5>{stays.length > 12 ? '12+ stays' : stays.length + ' stays'}</h5>
+      </div>
+      <Stays location={stays} />
+    </div>
+  );
+};
+
+export default App;
